5f76df67a0918f0000b1dff4264f8330edc716f8
GeekyMovies <121683432+GeekyMovies@users.noreply.github.com>
2023-01-02 23:25:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
-echo "Cloning Repo...."
-git clone https://github.com/GeekyMovies/LazyUrlHunterrBOT.git /LazyUrlHunterrBOT
-cd /LazyUrlHunterrBOT
-pip3 install -U -r requirements.txt
-echo "⚡️⚡️ Starting LazyUrlHunterrBOT..."
+git clone https://github.com/LazyDeveloperr/LazyUrlHunterrBOT
+# Install Packages
+pip3 install -r requirements.txt
+Edit configs.py with variables as given below then run bot
 python3 main.py
